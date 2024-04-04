97c6d8372d490a0243587f0766a03bbee7c6b521
Brett Logan <lindluni@github.com>
2024-04-03 21:11:44 -0400

    Update
    Signed-off-by: Brett Logan <lindluni@github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/nextjs-github-pages/vercel.svg"
+              src="/handbook/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
